1fb75f7511602065e84c00a7840bf449b5c98e1c
maheshreddi7 <maheshreddy.pannalaa@gmail.com>
2025-05-05 13:07:31 +0530

    changed name

--- Header.jsx
+++ Header.jsx
@@ -1,10 +1,10 @@
-import chefClaudeLogo from "./images/chef-claude-icon.png"
+import chefClaudeLogo from "./images/chef-claude-icon.png";
 
 export default function Header() {
-    return (
-        <header>
-            <img src={chefClaudeLogo}/>
-            <h1>Chef Claude</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src={chefClaudeLogo} />
+      <h1>Chef Racepie by GIEST</h1>
+    </header>
+  );
 }
